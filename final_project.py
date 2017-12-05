d967e6e1775cc08615da555d5b08f2ca1dafee98
Lilian Sheu <lilians@umich.edu>
2017-12-05 13:47:17 -0500

    added main python file

---
+++ final_project.py
@@ -0,0 +1,19 @@
+## SI 206 2017
+## Final Project
+
+## Name: Lilian Sheu
+## Discussion: Thursdays 3-4pm
+
+import unittest
+import itertools
+import collections
+
+# import API's
+# import tweepy
+# import twitter_info
+
+import json
+import sqlite3
+import re
+import sys
+import datetime

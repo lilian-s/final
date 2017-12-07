c2799a36c5b9a9e93607ccbf2d495bbadef3ab82
Lilian Sheu <lilians@umich.edu>
2017-12-06 22:38:25 -0500

    working on insta api

---
+++ insta_info.py
@@ -0,0 +1,6 @@
+client_id = '8acc370ee6844f55ba1f62fadf9b6b5d'
+client_secret = 'baa73e130f7a47cc916cabf127ff43d4'
+auth_url = 'https://api.instagram.com/oauth/authorize/?client_id=8acc370ee6844f55ba1f62fadf9b6b5d&redirect_uri=https://instagram.com/genapa_2018&response_type=code'
+redirect_uri = 'https://instagram.com/genapa_2018/'
+access_token = '1361891494.8acc370.89740af8cdb340fd821b93b1baf305c0'
+ 
